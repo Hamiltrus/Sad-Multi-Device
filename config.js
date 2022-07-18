27261b44beb401c64485aa53cff69b25bf12cb1b
Hamiltrus <109434307+Hamiltrus@users.noreply.github.com>
2022-07-18 15:26:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,16 +4,13 @@ import { watchFile, unwatchFile } from 'fs'
 import moment from 'moment-timezone'
 
 global.owner = [
-  ['+62 831-4339-37633'],
-  ['+1 760-891-4335'],
-  ['+84 82 430 4616'],
-  ['+84 34 979 0091'],
-  ['+62 818-0545-0249'],
-  ['+62 831-4339-37633', 'Letta', true],
-  ['17608914335', 'Papah-Chan', true],
-  ['+84 82 430 4616', 'Bot', true],
-  ['+84 34 979 0091', 'Johannes', true],
-  ['+62 818-0545-0249', 'Krisna', true],
+  ['+62 857-0673-5450'],
+  ['+62 857-0673-5450'],
+  ['+62 857-0673-5450'],
+  ['+62 857-0673-5450'],
+  ['+62 857-0673-5450'],
+  ['+62 857-0673-5450', 'verdihatoru', true],
+ 
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
